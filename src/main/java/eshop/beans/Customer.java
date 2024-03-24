79e5317347b9e49812fcb7cb07c376ef11ac986d
andrewfung729 <official@waiki.me>
2024-03-24 17:00:56 +0800

    reformat the models & beans

--- src/main/java/eshop/beans/Customer.java
+++ src/main/java/eshop/beans/Customer.java
@@ -1,44 +1,49 @@
 package eshop.beans;
 
 public class Customer {
-  private String contactName = "";
-  private String deliveryAddress = "";
-  private String ccName = "";
-  private String ccNumber = "";
-  private String ccExpiryDate = "";
+    private String contactName = "";
+    private String deliveryAddress = "";
+    private String ccName = "";
+    private String ccNumber = "";
+    private String ccExpiryDate = "";
 
-  public String getContactName() {
-    return contactName;
-    }
-  public void setContactName(String contactName) {
-    this.contactName = contactName;
+    public String getContactName() {
+        return contactName;
     }
 
-  public String getDeliveryAddress() {
-    return deliveryAddress;
-    }
-  public void setDeliveryAddress(String deliveryAddress) {
-    this.deliveryAddress = deliveryAddress;
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
     }
 
-  public String getCcName() {
-    return ccName;
-    }
-  public void setCcName(String ccName) {
-    this.ccName = ccName;
+    public String getDeliveryAddress() {
+        return deliveryAddress;
     }
 
-  public String getCcNumber() {
-    return ccNumber;
-    }
-  public void setCcNumber(String ccNumber) {
-    this.ccNumber = ccNumber;
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
     }
 
-  public String getCcExpiryDate() {
-    return ccExpiryDate;
+    public String getCcName() {
+        return ccName;
     }
-  public void setCcExpiryDate(String ccExpiryDate) {
-    this.ccExpiryDate = ccExpiryDate;
+
+    public void setCcName(String ccName) {
+        this.ccName = ccName;
     }
-  }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public String getCcExpiryDate() {
+        return ccExpiryDate;
+    }
+
+    public void setCcExpiryDate(String ccExpiryDate) {
+        this.ccExpiryDate = ccExpiryDate;
+    }
+}
